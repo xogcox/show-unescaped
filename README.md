da7bc6428edd1e2096c2545fd737685f17b605ff
Gregory Cox <gcox_gcox@proton.me>
2022-07-31 10:43:42 +0900

    Add documentation

---
+++ README.md
@@ -0,0 +1,114 @@
+# Changing how Haskell `show` and `print` escape Unicode characters
+
+The `show` and `print` functions (and the outputs in `GHCi`) replace
+all non-ASCII characters[^1] with escape codes. For example:
+
+```haskell
+ghci> '÷'
+'\247'
+```
+
+This is useful in some cases: perhaps you are printing user-entered
+text into a logfile, and want to make sure it is free of aberrations
+like [Zalgo text](https://en.wikipedia.org/wiki/Zalgo_text).
+But what if you have strings or objects containing accented characters,
+symbols, emoji or non-Latin scripts that you want to print verbatim?
+
+This module provides convenience functions for replacing `show` and `print`
+so that these characters are not escaped.
+
+## Choosing which function to use
+
+You have three choices to make:
+
+1) Which characters should be escaped?
+
+ * All non-ASCII characters (the same as `show` and `print`)
+ * Only unprintable characters
+
+2) What format should be used for escape codes?
+
+ * Decimal (like `"\247"`)
+ * Hexadecimal (like `"\xf7"`)
+
+3) Which function are you replacing?
+
+ * `print`: The function used to output results in `GHCi`
+ * `show`: The function that converts objects from a variety of types to the `String` type
+ * Other functions: If there is a function that behaves like `show`, or
+is implemented using`show`, you can change its escape characters by
+applying one of the functions listed as "conversion functions" to its output.
+
+Depending on the choices above, select the function to use from the following table:
+
+| Escape which? | All non-ASCII | All non-ASCII | Unprintable only | Unprintable only |
+Escape code format? | Decimal | Hexadecimal | Decimal | Hexadecimal |
+| --- | --- | --- | --- | --- |
+Replacement for `print` | `print` | `printWithHex` | `printUnescapePrintable` | `printUnescapePrintableWithHex` |
+Replacement for `show` | `show` | `showWithHex` | `showUnescapePrintable` | `showUnescapePrintableWithHex` |
+Conversion functions | `id` | `withHex` | `unescapePrintable` | `unescapePrintablewithHex` |
+
+Of course, the functions in the leftmost column are the standard functions, and `id` indicates that
+no conversion is necessary because `show` already does what you want.
+
+## Implementation
+
+The conversion functions are functions of type `String -> String` that simply
+parse their input to find escape codes and replace them if necessary.
+(Escape codes for ASCII characters, such as `'\NUL'` and `'\n'`, are left unchanged.)
+
+The other functions are defined using the conversion functions, for example:
+
+```haskell
+showUnescapePrintable = unescapePrintable . show
+
+printUnescapePrintable = putStrLn . unescapePrintable . show
+```
+
+## Replacing `print`
+
+Functions whose name starts with `print` are intended for use with the
+`interactive-print` option of `GHCi`, [as explained in the GHC User's Guide](https://downloads.haskell.org/~ghc/latest/docs/html/users_guide/ghci.html#using-a-custom-interactive-printing-function).
+For example, you can use the following command-line option to set the print function:
+
+```
+ghci -interactive-print=Unescaped.printUnescapePrintableWithHex Unescaped
+```
+
+You can also set this option by adding the following line to your [`.ghci` configuration file](https://downloads.haskell.org/~ghc/latest/docs/html/users_guide/ghci.html#the-ghci-files):
+
+```
+import Unescaped
+:set -interactive-print=Unescaped.printUnescapePrintableWithHex
+```
+
+## Replacing `show`
+
+Functions whose name starts with `show` are replacements for the `show` function.
+You can use them directly, or you can choose one to replace `show` with,
+for example by adding the following to the import section of a source file:
+
+```haskell
+import Unescaped (showUnescapePrintableWithHex)
+import Prelude hiding (show)
+import qualified Prelude as P (show)
+```
+
+and then add the following line to the body of the file:
+
+```haskell
+show = showUnescapePrintableWithHex
+```
+
+This replaces `show` but keeps the original `show` function
+available, renamed as `P.show`. To derive or implement the `Show` class
+for your own objects, you will need to use `P.show`.
+
+### Note on terminology: What are ASCII and non-ASCII characters?
+
+[^1]: In this explanation, "ASCII characters" refer to what the official
+Unicode terminology calls the Unicode Basic Latin Block, which
+consists of the first 128 Unicode characters (officially called
+"code points"), since these characters are identical to the ASCII
+character set. "Non-ASCII characters" means all characters outside
+this block.
--- Unescaped.hs
+++ Unescaped.hs
@@ -1,13 +1,63 @@
-module Unescaped
-    ( printUnescapePrintable
-    , showUnescapePrintable
-    , unescapePrintable
+{-|
+Module      : Unescaped
+Description : Convenience functions for changing how @show@ escapes characters
+Copyright   : (c) Gregory Cox
+License     : BSD-3
+Maintainer  : gcox_gcox@proton.me
+Stability   : experimental
+Portability : Portable
+
+This library provides a convenient way of configuring GHCi to print non-ASCII
+output, and, similarly, a variation on the @'Text.Show.show'@ function that
+does not escape non-ASCII characters.
+
+For example, you can replace @'System.IO.print'@ with @'printUnescapePrintable'@,
+which only escapes unprintable characters.
+See [Configuring GHCi's print function](#g:1) below for instructions on how
+to set this as the print function GHCi uses.
+
+@
+>>> printUnescapePrintable (Just \'÷\', \'±\')
+(Just \'÷\', \'±\')
+@
+
+== Compatibility with @Read@
+
+The output of all the functions should be compatible with @'Text.Read.read'@. That is,
+in the case of @'unescapePrintable'@, for example, for all @a@:
+
+prop> read (showUnescapePrintable a) == read (show a)
+prop> read (unescapePrintable a) == read a
+-}
+
+module Unescaped (
+      -- * Configuring GHCi's print function
+      -- $printFunctions1
+      -- $printFunctions2
+      -- $printFunctions3
+      -- $printFunctions4
+      -- $printFunctions5
+      printUnescapePrintable
     , printWithHex
-    , showWithHex
-    , withHex
     , printUnescapePrintableWithHex
+
+      -- * Show functions
+      -- $showFunctions1
+      -- $showFunctions2
+      -- $showFunctions3
+      -- $showFunctions4
+      -- $showFunctions5
+      -- $showFunctions6
+    , showUnescapePrintable
+    , showWithHex
     , showUnescapePrintableWithHex
+
+      -- * Conversion functions
+      -- $conversionFunctions
+    , unescapePrintable
+    , withHex
     , unescapePrintableWithHex
+
     ) where
 
 import Data.Char (ord, chr, isPrint, isDigit, isHexDigit)
@@ -17,6 +67,63 @@ import Data.Maybe (listToMaybe, fromMaybe)
 import Numeric (readDec, showHex)
 import System.IO (putStrLn)
 
+-- $printFunctions1
+-- Functions whose name starts with @print@ are intended for use with the
+-- @interactive-print@ option of GHCi,
+-- [as explained in the GHC User's Guide](https://downloads.haskell.org/~ghc/latest/docs/html/users_guide/ghci.html#using-a-custom-interactive-printing-function).
+-- For example, you can use the following command-line option to set the print function:
+
+-- $printFunctions2
+-- @
+-- ghci -interactive-print=Unescaped.printUnescapePrintableWithHex Unescaped
+-- @
+
+-- $printFunctions3
+-- You can also set this option by adding the following line to your [@.ghci@ configuration file](https://downloads.haskell.org/~ghc/latest/docs/html/users_guide/ghci.html#the-ghci-files):
+
+-- $printFunctions4
+-- @
+-- import Unescaped
+-- :set -interactive-print=Unescaped.printUnescapePrintableWithHex
+-- @
+-- $printFunctions5
+-- There are the following printing functions:
+
+-- $showFunctions1
+-- Functions whose name starts with @show@ are replacements for @'Text.Show.show'@.
+-- You can use them directly, or you can choose one to replace @'Text.Show.show'@ with,
+-- for example by adding the following to the import section of a source file:
+
+-- $showFunctions2
+-- @
+-- import Unescaped (showUnescapePrintableWithHex)
+-- import Prelude hiding (show)
+-- import qualified Prelude as P (show)
+-- @
+
+-- $showFunctions3
+-- and then add the following line to the body of the file:
+
+-- $showFunctions4
+-- @
+-- show = showUnescapePrintableWithHex
+-- @
+
+-- $showFunctions5
+-- This replaces @'Text.Show.show'@ but keeps the original @'Text.Show.show'@ function
+-- available, renamed as @P.show@. To derive or implement the @'Text.Show.Show'@ class
+-- for your own objects, you will need to use @P.show@.
+
+-- $showFunctions6
+-- There are the following three show functions:
+
+-- $conversionFunctions
+-- These are the basic functions used for parsing. They are less likely to be useful,
+-- but if a function that uses @'Text.Show.show'@ internally has unwanted escape
+-- sequences output by @'Text.Show.show'@, you can use these functions to remove or
+-- replace those escape sequences.
+
+
 data ParseResult
     = PassThrough
     | AsChar Char String
@@ -25,12 +132,36 @@ data ParseResult
 
 type EscapingPolicy = Char -> String -> ParseResult
 
+-- | Like @'System.IO.print'@, but display printable characters unchanged
+--   instead of escaping them.
+-- 
+-- >>> print ['\x9d'..'\xa5']
+-- "\157\158\159\160\161\162\163\164\165"
+-- >>> printUnescapePrintable ['\x9d'..'\xa5']
+-- "\157\158\159 ¡¢£¤¥"
+-- 
 printUnescapePrintable :: Show a => a -> IO ()
 printUnescapePrintable = putStrLn . showUnescapePrintable
 
+-- | Like @'Text.Show.show'@, but display printable characters unchanged
+--   instead of escaping them.
+-- 
+-- >>> putStrLn $ show ['\x9d'..'\xa5']
+-- "\157\158\159\160\161\162\163\164\165"
+-- >>> putStrLn $ show UnescapePrintable ['\x9d'..'\xa5']
+-- "\157\158\159 ¡¢£¤¥"
+-- 
 showUnescapePrintable :: Show a => a -> String
 showUnescapePrintable = unescapePrintable . show
 
+-- | Convert the output of @'Text.Show.show'@ to display printable
+--   characters unchanged instead of escaping them.
+-- 
+-- >>> putStrLn $ show ['\x9d'..'\xa5']
+-- "\157\158\159\160\161\162\163\164\165"
+-- >>> putStrLn $ unescapePrintable $ show ['\x9d'..'\xa5']
+-- "\157\158\159 ¡¢£¤¥"
+-- 
 unescapePrintable :: String -> String
 unescapePrintable = unescapeUnicodeGeneral decPrintable
 
@@ -38,24 +169,75 @@ decPrintable :: EscapingPolicy
 decPrintable c s =
     if isPrint c then AsChar c s else PassThrough
 
+-- | Like @'System.IO.print'@, but use hexadecimal instead of decimal
+--   in escape codes.
+-- 
+-- >>> print ['\x9d'..'\xa5']
+-- "\157\158\159\160\161\162\163\164\165"
+-- >>> printWithHex ['\x9d'..'\xa5']
+-- "\x9d\x9e\x9f\xa0\xa1\xa2\xa3\xa4\xa5"
+-- 
 printWithHex :: Show a => a -> IO ()
 printWithHex = putStrLn . showWithHex
 
+-- | Like @'Text.Show.show'@, but use hexadecimal instead of decimal
+--   in escape codes.
+-- 
+-- >>> putStrLn $ show ['\x9d'..'\xa5']
+-- "\157\158\159\160\161\162\163\164\165"
+-- >>> putStrLn $ showWithHex ['\x9d'..'\xa5']
+-- "\x9d\x9e\x9f\xa0\xa1\xa2\xa3\xa4\xa5"
+-- 
 showWithHex :: Show a => a -> String
 showWithHex = withHex . show
 
+-- | Convert the output of @'Text.Show.show'@ to use hexadecimal
+--   instead of decimal in escape codes.
+-- 
+-- >>> putStrLn $ show ['\x9d'..'\xa5']
+-- "\157\158\159\160\161\162\163\164\165"
+-- >>> putStrLn $ withHex $ show ['\x9d'..'\xa5']
+-- "\x9d\x9e\x9f\xa0\xa1\xa2\xa3\xa4\xa5"
+-- 
 withHex :: String -> String
 withHex = unescapeUnicodeGeneral decToHex
 
 decToHex :: EscapingPolicy
 decToHex c s = AsCode (toHexCode c s) s
 
+-- | Like @'System.IO.print'@, but display printable characters unchanged
+--   instead of escaping them, and use hexadecimal instead of decimal
+--   in escape codes.
+-- 
+-- >>> print ['\x9d'..'\xa5']
+-- "\157\158\159\160\161\162\163\164\165"
+-- >>> printUnescapePrintableWithHex ['\x9d'..'\xa5']
+-- "\x9d\x9e\x9f ¡¢£¤¥"
+-- 
 printUnescapePrintableWithHex :: Show a => a -> IO ()
 printUnescapePrintableWithHex = putStrLn . showUnescapePrintableWithHex
 
+-- | Like @'Text.Show.show'@, but display printable characters unchanged
+--   instead of escaping them, and use hexadecimal instead of decimal
+--   in escape codes.
+-- 
+-- >>> putStrLn $ show ['\x9d'..'\xa5']
+-- "\157\158\159\160\161\162\163\164\165"
+-- >>> putStrLn $ show UnescapePrintableWithHex ['\x9d'..'\xa5']
+-- "\x9d\x9e\x9f ¡¢£¤¥"
+-- 
 showUnescapePrintableWithHex :: Show a => a -> String
 showUnescapePrintableWithHex = unescapePrintableWithHex . show
 
+-- | Convert the output of @'Text.Show.show'@ to display printable
+--   characters unchanged instead of escaping them, and use
+--   hexadecimal instead of decimal in escape codes.
+-- 
+-- >>> putStrLn $ show ['\x9d'..'\xa5']
+-- "\157\158\159\160\161\162\163\164\165"
+-- >>> putStrLn $ unescapePrintableWithHex $ show ['\x9d'..'\xa5']
+-- "\x9d\x9e\x9f ¡¢£¤¥"
+-- 
 unescapePrintableWithHex :: String -> String
 unescapePrintableWithHex = unescapeUnicodeGeneral hexPrintable
 
@@ -80,6 +262,12 @@ unescapeUnicodeGeneral convert s =
                   AsChar c' s' -> ([c'], removeEscape s')
                   AsCode x s   -> (x, s)
               where removeEscape s = fromMaybe s . stripPrefix "\\&" $ s
+                      -- "\&" marks the end of an escape sequence in ambiguous cases
+                      -- that occur when the escape is followed by a literal digit.
+                      -- For example, this differentiates ['\1000','9'] (shown as "\1000\&9")
+                      -- from ['\10009'] (shown as "\10009").
+                      -- When the escape sequence is replaced by a literal, it
+                      -- becomes unnecessary, and can be deleted.
                     selectOutput p = case p of
                         Nothing -> PassThrough
                         Just (c, s) -> convert c s
