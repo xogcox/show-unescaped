9d9aa1764f36dd226d38dcb6513b46e75dcf039d
Gregory Cox <gcox_gcox@proton.me>
2022-07-31 10:43:42 +0900

    Rework parsing and exclude codes above maximum

--- Unescaped.hs
+++ Unescaped.hs
@@ -5,7 +5,9 @@ module Unescaped
     ) where
 
 import Data.Char (ord, chr, isPrint, isDigit)
-import Data.List (foldl')
+import Data.List.NonEmpty (NonEmpty(..), nonEmpty)
+import Data.Maybe (listToMaybe)
+import Numeric (readDec)
 import System.IO (putStrLn)
 
 printUnescaped :: Show a => a -> IO ()
@@ -22,22 +24,26 @@ unescapeUnicode (c:s) =
         _ -> c : unescapeUnicode s
     where escapeCode [] = "\\" -- A final backslash is an illegal escape sequence.
                                -- However, pass it through instead of doing validation.
-          escapeCode cs@(c:s)
-              | isDigit c = splitInt cs
-              | otherwise = '\\' : c : unescapeUnicode s
-          splitInt s =
-              let (ds, s') = span isDigit s
-                  in case (testIntStr ds, s') of
-                         (Nothing, _) -> ('\\' : ds) ++ unescapeUnicode s'
-                         (Just c, '\\' : '&' : suffix) ->
-                                        c : unescapeUnicode suffix
-                         (Just c, _) -> c : unescapeUnicode s'
-          testIntStr :: String -> Maybe Char
-          testIntStr s
-              | isPrint ch = Just ch
-              | otherwise = Nothing
-              where ch = chr . readDec . map fromDigit $ s
-                        where fromDigit c = ord c - ord '0'
-                              readDec :: [Int] -> Int
-                              readDec = foldl' step 0
-                                  where step a b = a * 10 + b
+          escapeCode cs@(c:s) =
+              case parseDec cs of
+                  Nothing       -> '\\' : c : unescapeUnicode s
+                  Just (c', '\\' : '&' : suffix)
+                                -> c' : unescapeUnicode suffix
+                  Just (c', s') -> c' : unescapeUnicode s'
+          parseDec :: String -> Maybe (Char, String)
+          parseDec s
+              | tooManyDigits s = Nothing
+              | otherwise = listToMaybe (readDec s) >>= verifyCharInRange
+              where maxOrd :: Int
+                    maxOrd = ord (maxBound :: Char)
+                    tooManyDigits :: String -> Bool
+                    tooManyDigits = all isDigit . take (maxOrdDigits + 1)
+                        where maxOrdDigits = length . takeWhile (>0) .
+                                  iterate (`div` 10) $ maxOrd
+                    verifyCharInRange :: (Int, String) -> Maybe (Char, String)
+                    verifyCharInRange (i, s)
+                        | i > maxOrd        = Nothing
+                        | not . isPrint $ c = Nothing
+                        | otherwise         = Just (c, s)
+                        where c = chr i
+
